149ee896d11af66f1a0e6987e1d888922db62961
Abijith Kumar Anand <abijithk91@gmail.com>
2025-04-17 19:46:30 +0530

    Headers file

--- my_introduction_app/src/components/Header.js
+++ my_introduction_app/src/components/Header.js
@@ -2,11 +2,19 @@ import React, {Component} from "react";
 import './Header.css';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Define any state variables here if needed
+      title: "My React Header"
+    };
+  }
   render() {
     return (
       <div>
         <header className="header">
-           <center><h1> React Header </h1>  <input style={{fontSize: '20px'}} type="text" name="textbox" id="textbox" placeholder=" Search Anything"/> </center>
+          
+           <center><h1> React Header </h1>  <input style={{fontSize: '20px'}} type="text" name="textbox" id="textbox" placeholder=" Search Anything"/> <div>{this.state.title}</div> </center>
         </header>
     </div>
       
